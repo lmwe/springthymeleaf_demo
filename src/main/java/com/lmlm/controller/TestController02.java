d0ad52c947a0cf2b402d58ce255149cabddb3469
lmlm <1660777585@>
2021-02-09 12:19:21 +0800

    第一次提交代码

---
+++ src/main/java/com/lmlm/controller/TestController02.java
@@ -0,0 +1,17 @@
+package com.lmlm.controller;
+
+/**
+ * @ClassName TestController02
+ * @Author: lmlm
+ * @Description TODO
+ * @date 2021/2/9 12:15
+ * @Version 1.0
+ */
+
+public class TestController02 {
+
+    public void Test(){
+
+    }
+
+}
